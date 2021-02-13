667f75aa13e92544a3421be41b8621018ea0743a
Ahsan Tahseen <58149036+ahsantahseen@users.noreply.github.com>
2021-02-13 20:54:53 +0500

    first

--- app.js
+++ app.js
@@ -38,7 +38,7 @@ app.use('/products',productsRoutes);
 
 app.use('/orders',ordersRoutes);
 app.use('/',(req,res,next)=>{
-    res.status(200).json({headers:{...req.headers,...res.getHeaders(),...req.connection.remoteAddress,...req.cookies,...req.hostname}})
+    res.status(200).json({headers:{...req.headers,...res.getHeaders(),...req.connection.remoteAddress,...req.cookies}})
     console.log({...req.headers,...res.getHeaders()})
     console.log("ip",req.connection.remoteAddress);
     console.log(req.headers['x-forwarded-for']);
