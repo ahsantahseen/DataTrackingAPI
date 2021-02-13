d9f664b2b17c2feab0f66dc2d6ab8cb8600b0572
Ahsan Tahseen <58149036+ahsantahseen@users.noreply.github.com>
2021-02-13 21:48:25 +0500

    1

--- app.js
+++ app.js
@@ -50,7 +50,7 @@ app.use('/',(req,res,next)=>{
     extraheaders:req.cookies,
     locationResponse:{...body},
     deviceParams:{...r},
-    timeStamp:Date.now().toString()
+    timeStamp:Date.UTC()
 
 })
 });
